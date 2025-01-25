cde293f49fba34760442b5b67e5de043c7b13b4f
ryansen <109828952+ryansen@users.noreply.github.com>
2025-01-25 00:01:52 -0800

    Update global.js

--- global.js
+++ global.js
@@ -7,10 +7,10 @@ function $$(selector, context = document) {
 
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'resume/', title: 'Resume' },
-  { url: 'contact/', title: 'Contact' },
+  { url: 'https://ryansen.github.io/portfolio/', title: 'Home' },
+  { url: 'https://ryansen.github.io/portfolio/projects/', title: 'Projects' },
+  { url: 'https://ryansen.github.io/portfolio/resume/', title: 'Resume' },
+  { url: 'https://ryansen.github.io/portfolio/contact/', title: 'Contact' },
   { url: 'https://github.com/ryansen', title: 'Github' },
 ];
 
